acc097fadb3c5e3947949726ea76f8dfde3f6e0e
GreyishOne <rovictim@gmail.com>
2018-09-20 12:14:35 +0200

    Given Zuul code
    Added "World of Zuul" code

---
+++ Group13/src/group13/Group13.java
@@ -0,0 +1,17 @@
+package group13;
+
+/**
+ *
+ * @author Rasmus Willer
+ */
+public class Group13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+    
+}
